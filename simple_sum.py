08d11551124dc35249dfd714092f35dc65cc0bbc
GrimNej <grim9413@gmail.com>
2025-11-22 16:44:31 +0545

    Added two new python files for testing

---
+++ simple_sum.py
@@ -0,0 +1,7 @@
+def sum(a, b):
+    return a + b
+
+
+input_1 = input("Enter the first number for sum: ")
+input_2 = input("Enter the second number for sum: ")
+print(f"The Sum is: {sum(input_1, input_2)}")
